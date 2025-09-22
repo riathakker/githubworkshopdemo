42321322b441708700bdbea4e03158025dc3cccf
riathakker <133058177+riathakker@users.noreply.github.com>
2025-09-21 22:12:40 -0700

    Update main for workshop

--- main.java
+++ main.java
@@ -1,5 +1,3 @@
 public static void main(String[] args) {
-    CHRISTIAN_SMITH math = new CHRISTIAN_SMITH();
     System.out.println("Hello San Francisco! This is the main function.");
-    math.test();
 }
